6e0ccd2af663a63e5c95fab9c4664938ae01c8af
Anurag Karki <anuragkarkikarki79@gmail.com>
2024-05-29 16:32:59 +0545

    new function added

--- Identity_Management_System.sol
+++ Identity_Management_System.sol
@@ -2,73 +2,59 @@
 
 pragma solidity ^0.8.7;
 
-contract identity_management {
-    struct Identity {
+contract identity_management{
+// Creating the structure named Identity for public(one who is part of block chain)
+    struct Identity{
         address user;
         string name;
         bytes32 father_name;
         bytes32 phone_number;
         bool isregistered;
     }
-
-    struct Identity_unhashed {
+// Creating the structure named Identity_unhashed for owner(One who created the identity)
+    struct Identity_unhashed{
         address user;
         string name;
         string father_name;
         string phone_number;
         bool isregistered;
     }
-
-    mapping(address => Identity) identities;
-    mapping(address => Identity_unhashed) identities_unhashed;
-
-    address[] identity_list;
-
-    modifier onlyregisterd() {
-        require(
-            identities[msg.sender].isregistered == true &&
-                identities_unhashed[msg.sender].isregistered == true,
-            "Not_Registerd"
-        );
+// Map address to identity
+    mapping (address => Identity)  identities;
+    mapping (address => Identity_unhashed) identities_unhashed;
+// array of identity_list of type address
+    address [] identity_list;
+// modifier which checks wether the user is registered or not
+    modifier onlyregisterd(){
+        require(identities[msg.sender].isregistered==true && identities[msg.sender].isregistered==true,"Not_Registerd");
         _;
     }
-
-    function add_identity(
-        string memory name,
-        string memory father_name,
-        string memory phone_number
-    ) public {
-        require(!identities[msg.sender].isregistered, "Already_Registered");
+// function to add the identity of the user
+// this function takes three arguments name,father name and phone_number
+    function add_identity(string memory name,string memory father_name,string memory phone_number)public{
+        require(!identities[msg.sender].isregistered,"Already_Registered");
         bytes32 hashed_father_name = keccak256(abi.encodePacked(father_name));
         bytes32 hashed_phone_number = keccak256(abi.encodePacked(phone_number));
-        identities[msg.sender] = Identity({
-            user: msg.sender,
+                identities[msg.sender] = Identity({
+            user:msg.sender,
             name: name,
             father_name: hashed_father_name,
             phone_number: hashed_phone_number,
             isregistered: true
         });
         identities_unhashed[msg.sender] = Identity_unhashed({
-            user: msg.sender,
-            name: name,
-            father_name: father_name,
-            phone_number: phone_number,
-            isregistered: true
+            user:msg.sender,
+            name:name,
+            father_name:father_name,
+            phone_number:phone_number,
+            isregistered:true
         });
         identity_list.push(msg.sender);
     }
-
-    //function to update identity of the user
-    // this function also takes three aguments as add_identity() function
-    function update_indentity(
-        string memory name,
-        string memory father_name,
-        string memory phone_number
-    ) public {
-        require(identities[msg.sender].isregistered == true, "Not_Registered");
-
-        // using keccak256 hashing for security purpose so that only the owner can see details
-        // when any other person will try to look at the informations then hashed value will be returned
+//function to update identity of the user
+// this function also takes three aguments as add_identity() function
+    function update_indentity(string memory name,string memory father_name,string memory phone_number)public{
+        require(identities[msg.sender].isregistered && identities[msg.sender].isregistered==true,"Not_Registered");
         bytes32 hashed_father_name = keccak256(abi.encodePacked(father_name));
         bytes32 hashed_phone_number = keccak256(abi.encodePacked(phone_number));
         identities[msg.sender].name = name;
@@ -76,27 +62,36 @@ contract identity_management {
         identities[msg.sender].phone_number = hashed_phone_number;
         identities_unhashed[msg.sender].name = name;
         identities_unhashed[msg.sender].father_name = father_name;
-        identities_unhashed[msg.sender].phone_number = phone_number;
+        identities_unhashed[msg.sender].phone_number = phone_number;     
     }
-    // Function to get adresses of all the registered users by registered user only
+// function to check wether user is registered or not returns bool value
+    function isregistered(address user)public view returns(bool){
+        return identities[user].isregistered && identities_unhashed[user].isregistered;
+    }
+// function to get identity by users who are part of block chain
+    function getIdentity_by_other(address user) public view returns (string memory, bytes32 ,bytes32) {
+        Identity memory id = identities[user];
+        return (id.name, id.father_name,id.phone_number);
+    }
+
+    function getIdentity_by_owner(address user) public view returns (string memory, string memory, string memory){
+        require(msg.sender == user,"Sorry Invalid Owner!!!");
+        Identity_unhashed memory id = identities_unhashed[user];
+        return (id.name,id.father_name,id.phone_number);
+
+    }
+
     function getAllIdentities(address user) public view returns (address[] memory) {
         require(identities[user].isregistered && identities_unhashed[user].isregistered, "Sorry You are not part of this chain");
         return identity_list;
     }
-    // Other users can only see the name of the owner of the address
-    function getIdentity_by_other(
-        address user
-    ) public view returns (string memory, bytes32, bytes32) {
-        Identity memory id = identities[user];
-        return (id.name, id.father_name, id.phone_number);
-    }
-
-    // The owner of the address can see all of their information
-    function getIdentity_by_owner(
-        address user
-    ) public view returns (string memory, string memory, string memory) {
-        require(msg.sender == user, "Sorry Invalid Owner!!!");
-        Identity_unhashed memory id = identities_unhashed[user];
-        return (id.name, id.father_name, id.phone_number);
-    }
+    // function deletes(address user) public  returns (address[] memory){
+    //     if(identities[user].isregistered && identities_unhashed[user].isregistered){
+    //         return identity_list;}
+    //     else{
+    //         delete identity_list;
+    //         return identity_list;
+    //     }
+    // }
 }
+
